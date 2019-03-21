18d676b390ef8b0595f5384025c03cafcf2d2f8f
jdriscoll98 <41447166+jdriscoll98@users.noreply.github.com>
2019-03-21 17:14:22 -0400

    lots of comments

--- website/models.py
+++ website/models.py
@@ -6,6 +6,7 @@ import stripe
 
 
 class Company(models.Model):
+    # this model represents our clients
     name = models.CharField(max_length=100)
     owner = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
     stripe_public = models.CharField(max_length=100)
@@ -15,6 +16,7 @@ class Company(models.Model):
     class Meta:
         verbose_name_plural = 'Companies'
 
+    # custom save model that makes the name into a unique slug for urls
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
         super(Company, self).save(*args, **kwargs)
@@ -22,17 +24,24 @@ class Company(models.Model):
     def __str__(self):
         return self.name
 
+    # creates a stripe customer and also creates a Customer object for future
     def create_stripe_customer(self, token, phone_number):
-        stripe.api_key = self.stripe_secret
-        stripe_customer = stripe.Customer.create(source=token)
-        Customer.objects.create(
-                                company=self,
-                                phone_number=phone_number,
-                                stripe_id=stripe_customer['id']
-                                )
-        return str(stripe_customer['id'])
+        try:
+            stripe.api_key = self.stripe_secret
+            # token = bank account token from plaid
+            stripe_customer = stripe.Customer.create(source=token)
+            # stripe_customer is a dictionary response containing their customer id
+            Customer.objects.create(
+                                    company=self,
+                                    phone_number=phone_number,
+                                    stripe_id=stripe_customer['id']
+                                    )
+            return True
+        except:
+            return False
 
     def charge_customer(self, id, amount):
+        # charges the customer with a certain id using their bank account
         stripe.api_key = self.stripe_secret
         stripe.Charge.create(
             amount=amount,
@@ -41,6 +50,7 @@ class Company(models.Model):
         )
 
 class Customer(models.Model):
+    # local model to store customer id's and find the them by a phone number
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=15, unique=True)
     stripe_id = models.CharField(max_length=100, unique=True)
@@ -49,4 +59,4 @@ class Customer(models.Model):
         return str(self.company) + ' ' + str(self.id)
 
     def get_absolute_url(self):
-        return reverse_lazy('website:plaid_auth')
+        return reverse_lazy('website:plaid_auth', kwargs={'slug': self.company.slug})
--- website/views.py
+++ website/views.py
@@ -27,33 +27,40 @@ from pprint import pprint
 
 
 class TwillioViewMixin(object):
+    # class based decorator for twilio view
     @method_decorator(twilio_view)
     def dispatch(self, *args, **kwargs):
         return super(TwillioViewMixin, self).dispatch(*args, **kwargs)
 
-
+# make a payment page
+@login_required
 def homepage_view(request):
     context = {
+        # this page will be used when using the service as a "point of sale" service
+        # other wise a custom link will be put on their homepage witht their slug
         'company': Company.objects.get(owner=request.user)
     }
     return render(request, "website/homepage.html", context)
 
 
+# First view the user reaches
 class SMS(TwillioViewMixin, FormView):
     template_name = 'website/phone_form.html'
     form_class = PhoneNumberForm
 
+    # if form is valid ->
     def form_valid(self, form):
         phone_number = self.request.POST.get('phone_number')
-        code = random.randint(10000, 99999)
+        code = random.randint(10000, 99999) # generate a random code to send to user
         client = TwilioClient(settings.TWILIO_ACCOUNT_SID,
                               settings.TWILIO_AUTH_TOKEN)
-        response = client.messages.create(
+        # twilio way of sending messages
+        client.messages.create(
                  body='Your secure access code is %s' % (str(code)),
                  to=phone_number, from_=settings.TWILIO_PHONE_NUMBER)
-        self.request.session['code'] = code
-        self.request.session['phone_number'] = phone_number
-        return HttpResponseRedirect(self.get_success_url())
+        self.request.session['code'] = code # store code in session for auth page
+        self.request.session['phone_number'] = phone_number # store number in session to get/create customer once authenticated
+        return HttpResponseRedirect(self.get_success_url()) # GO to auth_page
 
     def get_success_url(self, **kwargs):
         return reverse_lazy('website:auth_page', kwargs={'slug': self.kwargs.get('slug')})
@@ -63,15 +70,20 @@ class AuthPage(FormView):
     template_name = 'website/auth_page.html'
     form_class = AuthCodeForm
 
+    # if form is valid
     def form_valid(self, form):
-        auth_code = self.request.POST.get('code')
-        if int(auth_code) == int(self.request.session['code']):
+        auth_code = self.request.POST.get('code') # their submitted code
+        if int(auth_code) == int(self.request.session['code']): # check if it matches the stored code
+            # once authenticated, look for a customer
             customer = Customer.objects.filter(phone_number=self.request.session['number']).first()
+            # i did filter().first, because there will only be one customer with that phone number but there may be none
             if customer:
-                return HttpResponseRedirect(self.get_success_url())
+                return HttpResponseRedirect(self.get_success_url()) # payment page
             else:
+                # if no customer, go to plaid auth
                 return HttpResponseRedirect(reverse_lazy('website:plaid_auth', kwargs={'slug': self.kwargs.get('slug')}))
         else:
+            # wrong code, return to page again
             return HttpResponseRedirect(reverse_lazy('website:auth_page'))
 
     def get_success_url(self, **kwargs):
@@ -79,35 +91,37 @@ class AuthPage(FormView):
 
 class PlaidAuth(View):
     def get(self, *args, **kwargs):
+        # renders plaid Link
         context = {
             'slug': self.kwargs.get('slug')
         }
         return render(self.request, 'website/plaid_auth.html', context)
 
-    def post(self, *args, **kwargs):
+    def post(self, *args, **kwargs): # this needs cleaning up
         client = PlaidClient(
                             client_id=settings.PLAID_CLIENT_ID,
                             secret=settings.PLAID_SECRET,
                             public_key=settings.PLAID_PUBLIC_KEY,
                             environment=settings.PLAID_ENV,
                             )
-        public_token = self.request.POST.get('public_token')
-        response = client.Item.public_token.exchange(public_token)
-        print(response)
-        access_token = response['access_token']
-        auth_response = client.Auth.get(access_token)
-        account_id = auth_response['accounts'][0]['account_id']
+        public_token = self.request.POST.get('public_token') # passed in ajax
+        response = client.Item.public_token.exchange(public_token) # exchange token with plaid to get access_token
+        auth_response = client.Auth.get(response['access_token']) # authenticate user
+        account_id = auth_response['accounts'][0]['account_id']  # authed bank account id
+        # get a stripe bank acocunt token to link to a customer
         stripe_response = client.Processor.stripeBankAccountTokenCreate(
                                                                 access_token,
                                                                 account_id
                                                                 )
-        token = stripe_response['stripe_bank_account_token']
-        company = Company.objects.get(slug=self.kwargs.get('slug'))
-        phone_number = self.request.session['phone_number']
-        id = company.create_stripe_customer(token, phone_number)
-        self.request.session['customer_id'] = id
+        bank_account_token = stripe_response['stripe_bank_account_token']
+        company = Company.objects.get(slug=self.kwargs.get('slug')) # get company to use method
+        phone_number = self.request.session['phone_number'] # retrieve number to create customer
+        if company.create_stripe_customer(bank_account_token, phone_number):
+            success = True
+        else:
+            success = False
         data = {
-            'success': True,
+            'success': success,
             'url': reverse_lazy('website:payment', kwargs={'slug': company.slug})
         }
         return JsonResponse(data)
@@ -119,12 +133,13 @@ class PaymentView(FormView):
     success_url = reverse_lazy('website:thanks')
 
     def form_valid(self, form):
+        # gather information to charge customer
         phone_number = self.request.session['number']
         amount = int(self.request.POST.get('amount')) * 100
         customer_id = Customer.objects.get(phone_number=phone_number).stripe_id
         company = Company.objects.get(slug=self.kwargs.get('slug'))
         company.charge_customer(customer_id, amount)
-        return HttpResponseRedirect(reverse_lazy('website:thanks'))
+        return HttpResponseRedirect(reverse_lazy('website:thanks')) #thank you page
 
 
 class ThankYouPage(TemplateView):
